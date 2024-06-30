cd4e1b8ea459cfdcdca86d9e2611c0580702971f
khubaib-gullo <khubaibgullo377@gmail.com>
2024-06-30 11:02:01 -0700

    update changes

--- src/gullo/views.py
+++ src/gullo/views.py
@@ -4,6 +4,8 @@ from gapp.models import Gapp
 
 
 def home(request):
+
+    print(request.path)
     context = {
         "name": "khubaib",
         "queryset": Gapp.objects.filter(path=request.path).count()
@@ -11,6 +13,6 @@ def home(request):
     print(Gapp.objects.all())
     Gapp.objects.create(path=request.path)
     print(Gapp.objects.filter(path=request.path))
-    print(request.path)
+    
     return render(request, "index.html", context)
 
